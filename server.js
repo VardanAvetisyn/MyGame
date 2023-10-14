484cc33cbe3eed4c77246516edbcf969560e0cd6
VardanAvetisyn <vardan.avetisyan1@tumo.org>
2023-10-14 11:31:43 +0400

    added server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+
+app.use(express.static('.'))
+app.get("/", function (req, res) {
+    res.redirect('index.html')
+})
+
+
+
+server.listen(3000, () => {
+    console.log('server in listening to port 3000');
+})
